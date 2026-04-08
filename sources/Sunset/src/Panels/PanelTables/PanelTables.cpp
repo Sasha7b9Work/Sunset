f1246dfa5757b62d51ada37537d4b3ff995001b5
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-08 15:39:17 +0300

    refact

--- sources/Sunset/src/Panels/PanelTables/PanelTables.cpp
+++ sources/Sunset/src/Panels/PanelTables/PanelTables.cpp
@@ -1,4 +1,4 @@
-// 2026/04/08 15:17:54 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+п»ї// 2026/04/08 15:17:54 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/PanelTables/PanelTables.h"
 
@@ -6,7 +6,7 @@
 PanelTables *ThePanelTables = nullptr;
 
 
-PanelTables::PanelTables(wxWindow *parent) : Panel(parent, L("Таблицы"))
+PanelTables::PanelTables(wxWindow *parent) : Panel(parent, L("РўР°Р±Р»РёС†С‹"))
 {
     ThePanelTables = this;
 }
--- sources/Sunset/src/Panels/PanelTables/PanelTables.h
+++ sources/Sunset/src/Panels/PanelTables/PanelTables.h
@@ -1,4 +1,4 @@
-// 2026/04/08 15:17:29 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+﻿// 2026/04/08 15:17:29 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "Panels/Panels.h"
 
 
