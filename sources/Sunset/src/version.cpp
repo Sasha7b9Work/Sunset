b44a0134ad9c13b233386146c06092dd040953d0
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-08 10:58:34 +0300

    refact

--- sources/Sunset/src/version.cpp
+++ sources/Sunset/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1563 ;
-const char *DATE_BUILD = "2026-04-08 10:57:56" ;
+unsigned int VERSION_BUILD = 1564 ;
+const char *DATE_BUILD = "2026-04-08 10:58:34" ;
