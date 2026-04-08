83be0266a7e3f70452ac0a9c4e742898978a5fce
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-08 12:23:58 +0300

    *

--- sources/Sunset/src/Panels/PanelDebug/Notebook/AnimatedImpulse.cpp
+++ sources/Sunset/src/Panels/PanelDebug/Notebook/AnimatedImpulse.cpp
@@ -26,7 +26,7 @@ void AnimatedImpulse::FuncDraw()
     {
         if (meter.ElapsedMS() > 10)
         {
-            x += (int)(2.0f * ((float)meter.ElapsedMS() / (repeat ? 10.0f : 2.5f)));
+            x += (int)(2.0f * ((float)meter.ElapsedMS() / (repeat ? 10.0f : 4.0f)));
             if (x > GetSize().x)
             {
                 x = 0;
