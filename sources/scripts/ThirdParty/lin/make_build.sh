5a621c2808dcddd7e39290d9ea4a376eeb00186f
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-14 14:46:59 +0300

    *

--- sources/scripts/ThirdParty/lin/make_build.sh
+++ sources/scripts/ThirdParty/lin/make_build.sh
@@ -2,7 +2,7 @@
 
 rm -R -f ./../../../generated/wxWidgets
 
-cmake ../../../ThirdParty/wxWidgets/CMakeLists.txt -B../../../generated/wxWidgets -G "CodeBlocks - Unix Makefiles" -DwxUSE_GTK=ON -DwxBUILD_SAMPLES=OFF -DwxBUILD_SHARED=OFF -DwxBUILD_STRIPPED_RELEASE=ON -DCMAKE_CXX_FLAGS_INIT="-pipe" -DCMAKE_C_FLAGS_INIT="-pipe"
+cmake ../../../ThirdParty/wxWidgets/CMakeLists.txt -B../../../generated/wxWidgets -G "CodeBlocks - Unix Makefiles" -DwxUSE_GTK=OFF  -DwxUSE_X11=ON -DwxBUILD_SAMPLES=OFF -DwxBUILD_SHARED=OFF -DwxBUILD_STRIPPED_RELEASE=ON -DCMAKE_CXX_FLAGS_INIT="-pipe" -DCMAKE_C_FLAGS_INIT="-pipe"
 
 cd ../../../generated/wxWidgets/
 cmake --build . -- -j$(nproc)
