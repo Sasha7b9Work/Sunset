5a61a86f8b7468663fbdc7f483b18d4fa153d5eb
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-23 10:19:48 +0300

    refact

--- sources/Sunset/src/Controls/AutoRebootDialog.h
+++ sources/Sunset/src/Controls/AutoRebootDialog.h
@@ -2,7 +2,6 @@
 #pragma once
 #pragma warning(push, 0)
 #include <wx/stattext.h>
-#include <wx/button.h>
 #include <wx/sizer.h>
 #pragma warning(pop)
 
@@ -12,28 +11,27 @@ class AutoRebootDialog : public wxDialog
 public:
     AutoRebootDialog(wxWindow *parent, const wxString &message, int countdownSeconds, void (*_func_on_finish)()) :
         wxDialog(parent, wxID_ANY, "Ошибка", wxDefaultPosition, wxSize(350, 170), wxCAPTION | wxCLOSE_BOX | wxRESIZE_BORDER),
-        m_countdown(countdownSeconds),
-        m_initialCountdown(countdownSeconds),
+        countdown(countdownSeconds),
+        initialCountdown(countdownSeconds),
         func_on_finish(_func_on_finish)
     {
         wxBoxSizer *mainSizer = new wxBoxSizer(wxVERTICAL);
 
         // Текст сообщения
-        m_messageText = new wxStaticText(this, wxID_ANY, message);
-        mainSizer->Add(m_messageText, 0, wxALL | wxALIGN_CENTER, 10);
+        messageText = new wxStaticText(this, wxID_ANY, message);
+        mainSizer->Add(messageText, 0, wxALL | wxALIGN_CENTER, 10);
 
         // Текст отсчета
-        m_countdownText = new wxStaticText(this, wxID_ANY,
-            wxString::Format(L("Перезагрузка через %d секунд"), m_countdown));
-        mainSizer->Add(m_countdownText, 0, wxALL | wxALIGN_CENTER, 10);
+        countdownText = new wxStaticText(this, wxID_ANY, wxString::Format(L("Перезагрузка через %d секунд"), countdown));
+        mainSizer->Add(countdownText, 0, wxALL | wxALIGN_CENTER, 10);
 
         // Кнопки
         wxBoxSizer *buttonSizer = new wxBoxSizer(wxHORIZONTAL);
-        m_okButton = new wxButton(this, wxID_OK, "Перезагрузить");
-        m_cancelButton = new wxButton(this, wxID_CANCEL, "Отмена");
+        okButton = new Button(this, L("Перезагрузить"));
+        cancelButton = new Button(this, L("Отмена"));
 
-        buttonSizer->Add(m_okButton, 0, wxALL, 5);
-        buttonSizer->Add(m_cancelButton, 0, wxALL, 5);
+        buttonSizer->Add(okButton, 0, wxALL, 5);
+        buttonSizer->Add(cancelButton, 0, wxALL, 5);
 
         mainSizer->Add(buttonSizer, 10, wxALIGN_CENTER | wxBOTTOM, 20);
 
@@ -41,40 +39,39 @@ public:
         wxWindowBase::Centre();
 
         // Настройка таймера
-        m_timer = new wxTimer(this, wxID_ANY);
-        m_timer->Start(1000); // Таймер срабатывает каждую секунду
+        timer = new wxTimer(this, wxID_ANY);
+        timer->Start(1000); // Таймер срабатывает каждую секунду
 
         // Привязка событий кнопок
-        m_okButton->Bind(wxEVT_BUTTON, &AutoRebootDialog::OnOk, this);
-        m_cancelButton->Bind(wxEVT_BUTTON, &AutoRebootDialog::OnCancel, this);
+        okButton->Bind(wxEVT_BUTTON, &AutoRebootDialog::OnOk, this);
+        cancelButton->Bind(wxEVT_BUTTON, &AutoRebootDialog::OnCancel, this);
 
         Bind(wxEVT_TIMER, &AutoRebootDialog::OnTimer, this);
     }
 
     ~AutoRebootDialog()
     {
-        if (m_timer && m_timer->IsRunning())
+        if (timer && timer->IsRunning())
         {
-            m_timer->Stop();
+            timer->Stop();
         }
     }
 
 private:
     void OnTimer(wxTimerEvent &)
     {
-        m_countdown--;
+        countdown--;
 
-        if (m_countdown <= 0)
+        if (countdown <= 0)
         {
-            m_timer->Stop();
+            timer->Stop();
             // Выполняем действие по достижении нуля
             OnCountdownFinished();
         }
         else
         {
             // Обновляем текст отсчета
-            m_countdownText->SetLabel(
-                wxString::Format(L("Перезагрузка через %d секунд"), m_countdown));
+            countdownText->SetLabel(wxString::Format(L("Перезагрузка через %d секунд"), countdown));
             Layout();
         }
     }
@@ -89,7 +86,7 @@ private:
 
     void OnOk(wxCommandEvent &)
     {
-        m_timer->Stop();
+        timer->Stop();
         EndModal(wxID_OK);
 
         func_on_finish();
@@ -97,17 +94,17 @@ private:
 
     void OnCancel(wxCommandEvent &)
     {
-        m_timer->Stop();
+        timer->Stop();
         EndModal(wxID_CANCEL);
     }
 
 private:
-    wxStaticText *m_messageText;
-    wxStaticText *m_countdownText;
-    wxButton *m_okButton;
-    wxButton *m_cancelButton;
-    wxTimer *m_timer;
-    int m_countdown;
-    int m_initialCountdown;
+    wxStaticText *messageText;
+    wxStaticText *countdownText;
+    Button       *okButton;
+    Button       *cancelButton;
+    wxTimer      *timer;
+    int           countdown;
+    int           initialCountdown;
     void (*func_on_finish)() = nullptr;
 };
