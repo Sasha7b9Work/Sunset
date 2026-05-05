d469bb1e4a548663e22cab380f95b98bc65fe35b
Sasha7b9Work <Phuntik-it@tut.by>
2026-05-05 10:19:31 +0300

    *

--- sources/scripts/lin/build.sh
+++ sources/scripts/lin/build.sh
@@ -3,7 +3,6 @@
 git pull
 dir=$PWD
 cd ../../generated/Linia
-cmake . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake 2>&1 | grep -i vcpkg
 cmake --build . -- -j$(nproc)
 cd $dir
 
--- sources/scripts/lin/full_build.sh
+++ sources/scripts/lin/full_build.sh
@@ -2,6 +2,6 @@
 
 rm -R -f ../../generated/Linia
 
-cmake ../../CMakeLists.txt -B../../generated/Linia -G "CodeBlocks - Unix Makefiles"
+cmake ../../CMakeLists.txt -B../../generated/Linia -G "CodeBlocks - Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 ./build.sh
