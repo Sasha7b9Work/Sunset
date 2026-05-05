86d45355499d9bfe6c51f6c464b6177d34c4acd8
Sasha7b9Work <Phuntik-it@tut.by>
2026-05-05 09:51:46 +0300

    Добавлена установка пакета vcpkg на Linux

--- sources/scripts/lin/install_soft.sh
+++ sources/scripts/lin/install_soft.sh
@@ -6,3 +6,4 @@ apt-get -y upgrade
 apt-get -y install nginx htop openssh-server openssh-client libssh-dev iptables-persistent mc links
 apt-get -y install cmake gcc make python3 python3-pip python3-venv build-essential gdb
 apt-get -y install libssl-dev libx11-dev libxrandr-dev mesa-common-dev libgl1-mesa-dev libasound2-dev
+apt-get -y install build-essential git curl zip unzip tar pkg-config
