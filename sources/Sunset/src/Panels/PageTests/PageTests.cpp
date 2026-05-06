66b8bffed69a0128127422204fd900b926bd6023
Sasha7b9Work <Phuntik-it@tut.by>
2026-05-06 14:39:31 +0300

    *

--- sources/Sunset/src/Panels/PageTests/PageTests.cpp
+++ sources/Sunset/src/Panels/PageTests/PageTests.cpp
@@ -37,9 +37,9 @@ PageTests::PageTests(Notebook *board, PageTests *&global) :
 
     panel_view_test = new PanelViewTest(hor_splitter, ThePanelViewTest);
 
-    vert_splitter->SplitHorizontally(panel_category, panel_library, 100);
+    vert_splitter->SplitHorizontally(panel_category, panel_library, 90);
 
-    hor_splitter->SplitVertically(vert_splitter, panel_view_test, 200);
+    hor_splitter->SplitVertically(vert_splitter, panel_view_test, 190);
 
     wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
     sizer->Add(hor_splitter, 1, wxEXPAND);
