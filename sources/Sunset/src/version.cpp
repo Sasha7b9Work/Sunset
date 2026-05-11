342c6fa4138a50bf6d8e562f3a6f632170c859ca
Sasha7b9Work <Phuntik-it@tut.by>
2026-05-11 13:27:01 +0300

    *

--- sources/Sunset/src/version.cpp
+++ sources/Sunset/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1793 ;
-const char *DATE_BUILD = "2026-05-11 13:26:19" ;
+unsigned int VERSION_BUILD = 1794 ;
+const char *DATE_BUILD = "2026-05-11 13:27:01" ;
