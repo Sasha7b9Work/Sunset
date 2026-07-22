56ddb4a95d4b87f1006291f34619da28ef9faa69
Sasha7b9Work <Phuntik-it@tut.by>
2026-07-22 11:18:49 +0300

    Добавлен Device в solutions

--- sources/Sunset/src/version.cpp
+++ sources/Sunset/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1848 ;
-const char *DATE_BUILD = "2026-07-21 17:32:51" ;
+unsigned int VERSION_BUILD = 1849 ;
+const char *DATE_BUILD = "2026-07-22 11:18:49" ;
